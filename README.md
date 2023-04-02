dfab4cc5420702afc65fbf3deab63c1d8565d627
ccwillem <105742080+ccwillem@users.noreply.github.com>
2023-04-02 13:22:03 +0200

    Added bruteforce functionality with a wordlist

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Dangling Domains
 
+## Update 02 April 2023
+Added functionality to bruteforce (dictionary attack) subdomains with a wordlist. Script only checks if a CNAME is present for each subdomain. If a CNAME exists, it will check if the CNAME resolves to an IP address.
+
 ## Purpose
 Dangling domains are subdomains that have a CNAME record but the resource it refers to does not exist anymore. If these resource referred to in the CNAME is a third-party resource like Azure, GCP or AWS, this resource can be created by anybody and the subdomain is thereby hijacked.
 
@@ -21,6 +24,11 @@ Use the -l option to provide a list of subdomains in a file, one per line.
 python3 danglingdomains.py -l path/to/file.txt
 ```
 
+Use the -b option together with the -w and -d options to bruteforce subdomains using a wordlist.
+```
+python3 danglingdomains.py -d example.com -b -w /usr/share/wordlists/Seclists/Discovery/DNS/dns-Jhaddix.txt
+```
+
 ## Options
 The following options are available:
 
@@ -30,4 +38,7 @@ Short format	| Long format	| Description
 -l			 	| --list		| path to file containing list with subdomains, separated by newlines
 -v				| --verbose		| show verbose output of each subdomain, CNAME and IP address
 -j				| --json		| show output in json format. Only dangling domains will be shown, no verbose info will be included
+-b              | --brute       | use a wordlist to bruteforce subdomains and find CNAMES. to be used together with -d and -w
+-w              | --wordlist    | path to file with wordlist containing prefixes for subdomain breuteforcing
 -h				| --help		| show the help message and exi
+
--- danglingdomains.py
+++ danglingdomains.py
@@ -8,24 +8,23 @@ import argparse
 import validators
 import dns.resolver, dns.exception, dns.rdatatype
 import sys
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--domain", action="store", help="enter the domain to search for")
-parser.add_argument("-l", "--list", action="store", help="path to file containing list with subdomains, separated by newlines")
-parser.add_argument("-v", "--verbose", action="store_true", help="show verbose output of each subdomain, CNAME and IP address")
-parser.add_argument("-j", "--json", action="store_true", help="show output in json format. Only dangling domains will be shown, no verbose info will be included")
+parser.add_argument("-d", "--domain", action="store", help="Enter the domain you would like to search for.")
+parser.add_argument("-l", "--list", action="store", help="(rel) path to file containing list with subdomains separated by newlines")
+parser.add_argument("-b", "--brute", action="store_true", help="Use a wordlist to bruteforce subdomains and find CNAMES. to be used together with -d and -w")
+parser.add_argument("-v", "--verbose", action="store_true", help="Show verbose output of each subdomain, cname and ip address")
+parser.add_argument("-w", "--wordlist", action="store", help="path to file with wordlist containing prefixes for subdomain breuteforcing")
+parser.add_argument("-j", "--json", action="store_true", help="Show output in json format. Only dangling domains will be shown, no verbose info will be included.")
 
 args = parser.parse_args()
 
-search_domain = args.domain
+search_domain = args.domain ## list of positional first arguments: domain name
 input_list = []
 json_dangling = {'danglingDomains': []}
-
-
-if not search_domain and not args.list:
-    print('Specify input domain or a file with subdomains')
-    quit()
-
 
 if not args.json:
     print('''
@@ -40,29 +39,39 @@ if not args.json:
        `.,'
         /\`.   ,-._
             `-'     
-                    By ccwillem, 2022
+                    By Willem, 2022
 
 Find forgotten subdomains that are still floating in the air....
 ''')
-
+          
+if not search_domain and not args.list:
+    print('Specify input domain or a file with subdomains')
+    quit()
 
 if args.list:
     with open(args.list, 'r') as f:
         first_line = f.readline()
         lines = f.readlines()
-    non_empty_lines = [line.strip() for line in lines if line.strip() != ""]
-    input_list += non_empty_lines
+    non_empty_lines = [line.strip() for line in lines if line.strip() != ""] ## remove empty lines, and leading and trailing whitespace
+    input_list += non_empty_lines ## append targets from input list file to targets from command line input
     uniq_input_list = list(set(input_list))
     results = uniq_input_list
+
+if args.brute:
+    with open(args.wordlist, 'r') as f:
+        first_line = f.readline()
+        lines = f.readlines()
+    non_empty_lines = [line.strip() for line in lines if line.strip() != ""] ## remove empty lines, and leading and trailing whitespace
+    input_list += non_empty_lines ## append targets from input list file to targets from command line input
+    uniq_input_list = list(set(input_list))
+    subs_to_check = []
+    for item in uniq_input_list:
+        subs_to_check.append(f"{item}.{search_domain}")
+    results = subs_to_check
     if args.verbose:
-            print(f"\033[93m[ + ]\033[0m   In total \033[92m{len(results)}\033[0m unique subdomains are listed in file\n")
-            print("\033[93m[ + ]\033[0m    Checking for CNAME's...\n")
-    else:
-        if not args.json:
-            print(f"{len(results)} subdomains to check\n")
+        print(f"\033[93m[ + ]\033[0m   Bruteforcing subdomains for \033[92m{search_domain}\033[0m using wordlist \033[92m{args.wordlist}\033[0m\.\n")
 
-
-if args.domain:
+if args.domain and not args.brute:
     crt_url = "https://crt.sh/?Identity="+search_domain+"&output=json"
     if args.verbose:
         print("\033[93m[ + ]\033[0m   Retrieving certificate information from crt.sh\n")
@@ -70,7 +79,7 @@ if args.domain:
         response = requests.get(crt_url)
         if response.status_code == 200 and response.text:
             if args.verbose:
-                print("\033[93m[ + ]\033[0m   Successfully retrieved subdomains\n")
+                print("\033[93m[ + ]\033[0m   Successfully retrieved domains and subdomains\n")
         else:
             if args.verbose:
                 print("\033[91m[ - ]\033[0m   Error collecting information")
@@ -90,6 +99,7 @@ if args.domain:
             if validators.domain(subdomain) == True:
                 results.append(subdomain)
         results = list(set(results))
+        # print(results)
         if args.verbose:
             print(f"\033[93m[ + ]\033[0m   In total \033[92m{len(results)}\033[0m subdomains have been discovered for \033[92m{search_domain}\033[0m\n")
             print("\033[93m[ + ]\033[0m    Checking for CNAME's...\n")
@@ -97,52 +107,62 @@ if args.domain:
             if not args.json:
                 print(f"{len(results)} subdomains founds\n")
 
-
 domain_with_cname=[]
 dangling = []
 json_list =[]
 
+def cname_lookup(sub):
+    try:
+        answer = dns.resolver.resolve(sub, "CNAME")
+        if answer:
+            domain_with_cname.append(sub)
+            for rdata in answer:
+                cname = rdata.target
+                try:
+                    address = dns.resolver.resolve(cname, "A")
+                    for data in address:
+                        ip = data.address
+                        if args.verbose:
+                            tqdm.write("\033[92m{:50} {:5<} {:<50} {:<5} {:<0}\033[0m".format((str(sub)), str("--->"), str(cname), str("--->"),str(ip)))
+                except dns.exception.DNSException:
+                    if args.json:
+                        json_list.append(sub)
+                    else:
+                        dangling.append("{:50} {:5<} {:<50} {:<5} {:<0}".format((str(sub)), str("--->"), str(cname), str("--->"),str("???")))
+                        if args.verbose:
+                            tqdm.write("\033[91m{:50} {:5<} {:<50} {:<5} {:<0}\033[0m".format((str(sub)), str("--->"), str(cname), str("--->"),str("???")))
+                    continue               
+    except dns.exception.DNSException:
+        pass
+
+
 
 if len(results) != 0:
-    for sub in results:
-        try:
-            answer = dns.resolver.resolve(sub, "CNAME")
-            if answer:
-                domain_with_cname.append(sub)
-                for rdata in answer:
-                    cname = rdata.target
-                    try:
-                        address = dns.resolver.resolve(cname, "A")
-                        for data in address:
-                            ip = data.address
-                            if args.verbose:
-                                print("\033[92m{:50} {:5<} {:<50} {:<5} {:<0}\033[0m".format((str(sub)), str("--->"), str(cname), str("--->"),str(ip)))
-                    except dns.exception.DNSException:
-                        if args.json:
-                            json_list.append(sub)
-                        else:
-                            dangling.append("{:50} {:5<} {:<50} {:<5} {:<0}".format((str(sub)), str("--->"), str(cname), str("--->"),str("???")))
-                        continue               
-        except dns.exception.DNSException:
-            pass
+    pbar = tqdm(total=len(results), colour="yellow")
+    with ThreadPoolExecutor(max_workers=50) as pool:
+        futures = [pool.submit(cname_lookup, sub) for sub in results]
+        for _ in as_completed(futures):
+            pbar.update(n=1)    
+    pool.shutdown()
+            
+        
 
+        
 
 json_dangling['danglingDomains'] = json_list
-
-
 if args.json:
     print(json.dumps(json_dangling, indent=4))
 else:
     if len(domain_with_cname) == 0:
-        print("\n\033[93m[ + ]    No subdomains with CNAME record found. Better luck next time!\033[0m\n")
+        tqdm.write("\n\033[93m[ + ]    No subdomains with CNAME record found. Better luck next time!\033[0m\n")
     else:
         if len(dangling) == 0:
-            print("\n\033[93m[ + ]    All CNAME's resolve to an IP address. Better luck next time!\033[0m\n")
+            tqdm.write("\n\033[93m[ + ]    All CNAME's resolve to an IP address. Better luck next time!\033[0m\n")
         else:
-            print("\n\033[91m[ ! ]    Discovered the following dangling domains:\033[0m\n")
+            tqdm.write("\n\033[91m[ ! ]    Discovered the following dangling domains:\033[0m\n")
             for each in dangling:
-                print(f"\033[91m{each}\033[0m")
-            print("\n")
+                tqdm.write(f"\033[91m{each}\033[0m")
+            tqdm.write("\n")
 
 
 
